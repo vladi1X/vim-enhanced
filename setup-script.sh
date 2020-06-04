7abe340f0a0e9ec4edea37d204dc4d748ad7154e
vladi <vladi.werz@gmail.com>
2020-06-04 16:35:06 +0200

    choose if cloned dir should be deleted

--- setup-script.sh
+++ setup-script.sh
@@ -1,21 +1,37 @@
 #!/bin/bash
 
-#script to clone necessary github projects
 cd ~
+
+read -r -p "Do you want to keep the vim-enhanced directory? (y/N): " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+then
+    cloneNecessary
+    vim -c 'PluginInstall' -c 'qa!'
+else
+    mv vim-enhanced/.vim .
+    mv vim-enhanced/.vimrc .
+    rm -rf vim-enhanced/
+    cloneNecessary
+    vim -c 'PluginInstall' -c 'qa!'
+fi
+
 
 cp -a vim-enhanced/. .
 rm -rf vim-enhanced/
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-git clone https://github.com/preservim/nerdtree.git ~/.vim/pack/vendor/start/nerdtree
-vim -u NONE -c "helptags ~/.vim/pack/vendor/start/nerdtree/doc" -c q
+function cloneNecessary{
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-git clone https://github.com/itchyny/lightline.vim ~/.vim/pack/plugins/start/lightline
+    git clone https://github.com/preservim/nerdtree.git ~/.vim/pack/vendor/start/nerdtree
+    vim -u NONE -c "helptags ~/.vim/pack/vendor/start/nerdtree/doc" -c q
 
-mkdir -p ~/.vim/pack/tpope/start
-cd ~/.vim/pack/tpope/start
-git clone https://tpope.io/vim/eunuch.git
-vim -u NONE -c "helptags eunuch/doc" -c q
+    git clone https://github.com/itchyny/lightline.vim ~/.vim/pack/plugins/start/lightline
+
+    mkdir -p ~/.vim/pack/tpope/start
+    cd ~/.vim/pack/tpope/start
+    git clone https://tpope.io/vim/eunuch.git
+    vim -u NONE -c "helptags eunuch/doc" -c q
+}
 
 vim -c 'PluginInstall' -c 'qa!'
